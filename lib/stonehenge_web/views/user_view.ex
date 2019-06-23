9d90cb179a38621f7e75d232697780f5df8c9deb
Dannie Magalhaes <seliksy@gmail.com>
2019-06-22 22:27:17 -0300

    Add endpoint /users

---
+++ lib/stonehenge_web/views/user_view.ex
@@ -0,0 +1,19 @@
+defmodule StonehengeWeb.UserView do
+  use StonehengeWeb, :view
+  alias StonehengeWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id,
+      email: user.email,
+      is_active: user.is_active,
+      balance: user.balance}
+  end
+end
